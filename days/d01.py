dcb76876154938afa0c8358c825efe1847194701
mpironio <mpironio@novo.space>
2025-12-01 16:07:42 -0300

    segunda D:

--- days/d01.py
+++ days/d01.py
@@ -1,6 +1,8 @@
-
+import os
 def star1():
-    with open('d01.txt', 'r') as f:
+    with open('./inputs/d01.txt', 'r') as f:
+        global starting
+        global count
         while l := f.readline():
             value = 0
             if l[0] == 'L':
@@ -14,29 +16,30 @@ def star1():
 def star2():
     global starting
     global count
-    with open('d01.txt', 'r') as f:
+    with open('./inputs/d01_test.txt', 'r') as f:
         while l := f.readline():
-            value = 0
-            
-            if l[0] == 'L':
-                value = - int(l[1:])
-            if l[0] == 'R':
-                value = int(l[1:])
-            #print('value ', value)
-            sv = starting + value
-            #print('sv ', starting, value)
-            if value > 99 or value < -99:
-                count+=abs(value//100)
-                value = value % 100
-            if starting != 0 and (sv > 99 or sv < 0):
-                count+=1
-             #   print('count: ', count)
-            starting = (starting + value) % 100
-            #print('st' + str(starting))
-            if starting == 0:
-                count+=1
-             #   print('count: ', count)
+            value = (-1 if l[0] == 'L' else 1) * int(l[1:])
 
+            
+            if value not in range(-99, 100):
+                count+= abs(value) // 100
+                value = value % 100
+
+            sv = starting + value
+
+            if (starting + value) % 100 == 0:
+                count+=1
+
+            elif (starting + value) not in range(0, 100) and (starting + value) % 100 != 0:
+                count += 1
+
+            starting = sv % 100
+            print('starting', starting, count)
+
+#starting = 50
+#count = 0
+#star1()
+#print(count)
 starting = 50
 count = 0
 star2()
